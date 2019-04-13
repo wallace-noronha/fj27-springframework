969ced4469e30d7a40af30a3806666aa240199f5
Wallace Vidal <wallace.noronha@hotmail.com>
2019-04-13 09:23:43 -0300

    Acrescentando logica para acesso ao banco de dados

---
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -0,0 +1,27 @@
+package br.com.alura.forum.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
+import br.com.alura.forum.model.topic.domain.Topic;
+import br.com.alura.forum.repository.TopicRepository;
+
+@RestController
+public class TopicController {
+
+	@Autowired
+	private TopicRepository topicRepository;
+	
+	@GetMapping(value = "api/topics", produces= MediaType.APPLICATION_JSON_VALUE)
+	public List<TopicBriefOutputDto> listTopics(){
+
+		List<Topic> topics = topicRepository.findAll();
+		return TopicBriefOutputDto.listFromTopics(topics);
+	}
+	
+}
